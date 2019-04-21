8d224321730b0b2845cc08ac4bbdd96aa8d3dec1
linkean <741888784@qq.com>
2019-04-21 22:12:43 +0800

    加入生命周期

--- front/blog/src/components/BlogShowPage.vue
+++ front/blog/src/components/BlogShowPage.vue
@@ -7,71 +7,76 @@
       v-bind:title="blog.title"
       v-bind:content="blog.content"
       v-bind:blog_id="blog.blog_id"
-      v-bind:comments="blog.comments">
-    </blog>
+      v-bind:comments="blog.comments"
+    ></blog>
     <el-pagination
       @size-change="handleSizeChange"
       @current-change="handleCurrentChange"
       :current-page="currentPage"
       :page-size="pageSize"
       layout="total, prev, pager, next, jumper"
-      :total="pageTotal">
-    </el-pagination>
+      :total="pageTotal"
+    ></el-pagination>
   </div>
 </template>
 
 <script>
-  import Blog from '@/components/BlogDetail'
-  const myblog = {
-    name:"BlogShowPage",
-    data: function(){
-      return {
-        currentPage: 1,
-	pageSize: 1,
-	pageTotal: 5,
-	blogs: []
-      }  
-    },
-    components:{
-      'blog': Blog
-    },
-    methods: {
-        show_my_blogs: function(){
-	    this.$http.get("/api/show").then(function(jsondata){
-	        blog_form_body = jsondata.body;
-		blog_form_data = []
-		console.log(blog_form_body[0]);
-		for(var i=0; i<blog_form_body.length; i++){
-		    blog_form_data.push({
-			"blog_id": blog_form_body[i].id,
-		        "title": blog_form_body[i].blog.title, 
-			"content": blog_form_body[i].blog.content,
-			"comments": blog_form_body[i].comments
-		    });
-		}
-		this.pageTotal = blog_form_body.length;
-		this.total_blogs = blog_form_data;
-		this.blogs = [this.total_blogs[this.currentPage-1]];
-	      },function(res){
-	        console.log(res);
-		alert("Error: "+ res);
-	      });
-        },
-	handleCurrentChange: function(val){
-	    this.currentPage = val;
-	    this.blogs = [this.total_blogs[val-1]];
-	},
-	handleSizeChange: function(val){
-	    this.pageSize = val;
-	}
-      }
+import Blog from "@/components/BlogDetail";
+const myblog = {
+  name: "BlogShowPage",
+  data: function() {
+    return {
+      currentPage: 1,
+      pageSize: 1,
+      pageTotal: 5,
+      blogs: []
     };
-    export default myblog;
-    console.log(myblog);
-    myblog.methods.show_my_blogs();
+  },
+  components: {
+    blog: Blog
+  },
+  methods: {
+    show_my_blogs: function() {
+      this.$http.get("/api/show").then(
+        function(jsondata) {
+          blog_form_body = jsondata.body;
+          blog_form_data = [];
+          console.log(blog_form_body[0]);
+          for (var i = 0; i < blog_form_body.length; i++) {
+            blog_form_data.push({
+              blog_id: blog_form_body[i].id,
+              title: blog_form_body[i].blog.title,
+              content: blog_form_body[i].blog.content,
+              comments: blog_form_body[i].comments
+            });
+          }
+          this.pageTotal = blog_form_body.length;
+          this.total_blogs = blog_form_data;
+          this.blogs = [this.total_blogs[this.currentPage - 1]];
+        },
+        function(res) {
+          console.log(res);
+          alert("Error: " + res);
+        }
+      );
+    },
+    handleCurrentChange: function(val) {
+      this.currentPage = val;
+      this.blogs = [this.total_blogs[val - 1]];
+    },
+    handleSizeChange: function(val) {
+      this.pageSize = val;
+    }
+  },
+  mounted() {
+    this.show_my_blogs()
+  },
+};
+export default myblog;
+console.log(myblog);
 </script>
 <style>
-  .el-textarea__inner::placeholder {
-    color: #909399;
-  }
+.el-textarea__inner::placeholder {
+  color: #909399;
+}
 </style>
